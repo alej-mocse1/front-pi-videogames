07ae1aa5a0eac0cf70f8f0c47036738ff446f299
alej-mocse1 <alejandromcse01@gmail.com>
2022-12-14 01:28:27 -0300

    nuevos estilos en el componete pagInicial

--- src/componentes/PaginaInicial/paginaincial.jsx
+++ src/componentes/PaginaInicial/paginaincial.jsx
@@ -6,12 +6,25 @@ const PaginaInicial = () => {
 
      return (
         <div  className={style.divm}>
-         <div className={style.div} >
+         {/* <div className={style.div} >
            <img src="https://media.gq.com.mx/photos/60e9cc9c33c54bdef6761922/master/pass/E53RdQ5WQAIs7HC-scaled.jpg" className={style.img}/>   
          </div>
          <Link to='/paginaPrincipal'>
            <button className={style.home} type='submit'>home</button>
-         </Link> 
+         </Link>  */}
+         <div className={style.loader} >
+          <span>bienvenido</span>
+         </div>
+
+         <Link to='/paginaPrincipal'>
+         <button className={style.button}>
+         <span>Home</span>
+         <div className={style.top} ></div>
+         <div className={style.left} ></div>
+         <div className={style.bottom}></div>
+         <div className={style.right} ></div>
+        </button>
+        </Link> 
         </div>
       );
 }
